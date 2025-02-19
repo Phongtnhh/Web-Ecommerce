8f65522d6cd507ffd2291793c091ba9c37544c6b
Phongtnhh <166288264+Phongtnhh@users.noreply.github.com>
2025-02-19 13:14:43 +0700

    tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,30 +1,49 @@
 const Product = require("../../model/product.model");
-
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-
-    
     let find = {
-        deleted : false
+        deleted: false
     };
 
-    if(req.query.status){
+    if (req.query.status) {
         find.status = req.query.status;
     };
 
     let keyword = "";
-
-    if(req.query.keyword){
+    if (req.query.keyword) {
         keyword = req.query.keyword;
-
         const regex = new RegExp(keyword, "i");
         find.title = regex;
     }
 
-    const products = await Product.find(find);
-    
-    const productsJson = products.map(item => {return item});
+    // Phan trang
+    const countProducts = await Product.find(find);
+    let objectPagination = paginationHelper({
+        currentPage : 1,
+        limitItem: 4
+    },
+    req.query,
+    countProducts
+    );
 
-    res.json(productsJson);
+    
+
+    
+    // End Phan trang
+
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
+
+    const productsJson = products.map(item => {
+        return item
+    });
+
+    const result = {
+        products : productsJson,
+        keyword : req.query.keyword,
+        limit : objectPagination.currentPage
+    }
+
+    res.json(result);
 }
